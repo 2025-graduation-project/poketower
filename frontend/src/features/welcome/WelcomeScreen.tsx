c32377551b8371c7c330e35a1cbcc8dcc0357ccb
kima630 <kima063071@gmail.com>
2025-07-01 18:30:24 +0900

    💄 웰컴 스크린 UI 수정

--- frontend/src/features/welcome/WelcomeScreen.tsx
+++ frontend/src/features/welcome/WelcomeScreen.tsx
@@ -1,10 +1,17 @@
+import welcomeBg from '@/assets/img/welcomeBg.png';
+
 import { WelcomeScreenProps } from './type';
 
 const WelcomeScreen = ({ onStart }: WelcomeScreenProps) => {
   return (
-    <div>
-      <div>환영합니다</div>
-      <div onClick={() => onStart()}>시작</div>
+    <div
+      className="w-screen h-screen flex flex-col justify-center gap-40 bg-center bg-cover bg-no-repeat"
+      style={{ backgroundImage: `url(${welcomeBg})` }}
+    >
+      <div className="text-48 text-center">포켓타워에 오신 걸 환영합니다</div>
+      <div className="text-24 text-center" onClick={() => onStart()}>
+        게임 시작
+      </div>
     </div>
   );
 };
