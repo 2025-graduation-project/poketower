88d5cb4e59cfb499782ad9c691b33546cc97d82a
kima630 <kima063071@gmail.com>
2025-07-02 13:42:25 +0900

    💄 웰컴 스크린 게임 시작 버튼 CSS 추가

--- frontend/src/features/welcome/WelcomeScreen.tsx
+++ frontend/src/features/welcome/WelcomeScreen.tsx
@@ -9,7 +9,7 @@ const WelcomeScreen = ({ onStart }: WelcomeScreenProps) => {
       style={{ backgroundImage: `url(${welcomeBg})` }}
     >
       <div className="text-48 text-center">포켓타워에 오신 걸 환영합니다</div>
-      <div className="text-24 text-center" onClick={() => onStart()}>
+      <div className="cursor-pointer text-24 text-center" onClick={() => onStart()}>
         게임 시작
       </div>
     </div>
