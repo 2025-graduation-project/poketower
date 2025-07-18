c96def880f7d9723e27587bf34baf5e68fcd7a98
kima630 <kima063071@gmail.com>
2025-07-18 16:22:32 +0900

    ✨ 포켓몬 선택 기능 추가

--- frontend/src/features/starter/StarterSelect.tsx
+++ frontend/src/features/starter/StarterSelect.tsx
@@ -1,10 +1,57 @@
+import { useEffect, useState } from 'react';
+import { ChooseStarting, GetStartingPokemons } from '@wails/App';
 import { StarterSelectProps } from './type';
+import { Pokemon } from '@/entities/pokemon/type';
 
 const StarterSelect = ({ onConfirm }: StarterSelectProps) => {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [selected, setSelected] = useState<number[]>([]);
+
+  const pokemonSelect = (id: number) => {
+    if (selected.includes(id)) {
+      setSelected((prev) => prev.filter((pid) => pid !== id));
+    } else if (selected.length < 3) {
+      setSelected((prev) => [...prev, id]);
+    }
+  };
+
+  const handleConfirm = async () => {
+    if (selected.length !== 3) return;
+
+    try {
+      await ChooseStarting(selected);
+      console.log('포켓몬 선택 완료: ', selected);
+      onConfirm();
+    } catch (e) {
+      console.error('포켓몬 선택 실패: ', e);
+    }
+  };
+
+  useEffect(() => {
+    GetStartingPokemons().then((data) => {
+      setPokemons(data);
+    });
+  }, []);
+
   return (
-    <div>
+    <div className="w-screen">
       <div>포켓몬 세 마리를 고르기</div>
-      <div onClick={() => onConfirm()}>선택 완료</div>
+      <div className="grid grid-cols-auto-fill gap-16 p-20">
+        {pokemons.map((data: Pokemon) => (
+          <div
+            key={data.id}
+            className="flex flex-col"
+            style={{
+              border: selected.includes(data.id) ? '3px solid #f8b400' : '2px solid #ccc',
+            }}
+            onClick={() => pokemonSelect(data.id)}
+          >
+            <img className="w-60" src={new URL('../../assets/img/pokemon/' + data.id + '.png', import.meta.url).href} />
+            <div>{data.name}</div>
+          </div>
+        ))}
+      </div>
+      <div onClick={() => handleConfirm()}>선택 완료</div>
     </div>
   );
 };
