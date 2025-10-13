a087a54776cf2ebbc84e246c60279e968e27d7ee
october-03 <season_autumn@kakao.com>
2025-10-13 16:25:23 +0900

    :bug: 포켓몬 선택 시 타입, 스킬 선택 안되는 버그 수정

--- service/pokemon/chooseStarting.go
+++ service/pokemon/chooseStarting.go
@@ -4,11 +4,15 @@ import (
 	"poketower-client/service/battle"
 	"poketower-client/service/database"
 	"poketower-client/service/model"
+
+	"gorm.io/gorm"
 )
 
 func ChooseStarting(id []int) {
 	var pokemons []model.Pokemon
-	if err := database.DB.Where("id IN ?", id).Find(&pokemons).Error; err != nil {
+	if err := database.DB.Preload("Types").Preload("Moves", func(db *gorm.DB) *gorm.DB {
+		return db.Where("category <> ?", 3).Where("power IS NOT NULL")
+	}).Preload("Moves.Type").Where("id IN ?", id).Find(&pokemons).Error; err != nil {
 		panic("failed to find starting pokemons: " + err.Error())
 	}
 
