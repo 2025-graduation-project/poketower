3f6da491c511fa3666194beed2a3fe43e386e361
kima630 <kima063071@gmail.com>
2025-12-09 15:53:10 +0900

    ✨ 게임 초기화 기능 추가

--- frontend/src/features/welcome/WelcomeScreen.tsx
+++ frontend/src/features/welcome/WelcomeScreen.tsx
@@ -1,14 +1,24 @@
 import { WelcomeScreenProps } from './type';
+import { InitGame } from '@wails/App';
 import welcomeBg from '@/assets/img/welcomeBg.png';
 
 const WelcomeScreen = ({ onStart }: WelcomeScreenProps) => {
+  const handleGameStart = async () => {
+    try {
+      await InitGame();
+      onStart();
+    } catch (error) {
+      console.error('게임 초기화 실패:', error);
+    }
+  };
+
   return (
     <div
       className="w-screen h-screen flex flex-col justify-center gap-40 bg-center bg-cover bg-no-repeat"
       style={{ backgroundImage: `url(${welcomeBg})` }}
     >
       <div className="text-48 text-center">포켓타워에 오신 걸 환영합니다</div>
-      <div className="cursor-pointer text-24 text-center" onClick={() => onStart()}>
+      <div className="cursor-pointer text-24 text-center" onClick={() => handleGameStart()}>
         게임 시작
       </div>
     </div>
